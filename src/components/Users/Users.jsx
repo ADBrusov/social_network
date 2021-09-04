13b3f784f285cec205118c5433f64657a74e28f0
ADBrusov <ADBrusov96@yandex.ru>
2021-09-04 17:26:24 +0300

    Заменяет хардкод в компоненте на get запрос к серверу

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,44 +1,12 @@
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userAvatarSmall from '../../images/userAvatar_small.jpg';
 
 const Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        followed: false,
-        name: "Alexander",
-        status: "На собеседовании",
-        location: {country: 'Russia', city: 'Moscow/Novocheboksarsk',}
-      },
-      {
-        id: 2,
-        followed: true,
-        name: "Alex",
-        status: "На работе",
-        location: {country: 'Russia', city: 'Novocheboksarsk',}
-      },
-      {
-        id: 3,
-        followed: true,
-        name: "Nikita",
-        status: "На чилле, на расслабоне",
-        location: {country: 'Russia', city: 'Moscow',}
-      },
-      {
-        id: 4,
-        followed: true,
-        name: "Krus",
-        status: "На карантине",
-        location: {country: 'Russia', city: 'Moscow',}
-      },
-      {
-        id: 5,
-        followed: true,
-        name: "Mini-Krus",
-        status: "На учебе",
-        location: {country: 'Russia', city: 'Moscow',}
-      },
-    ]);
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      props.setUsers(response.data.items);
+    });
   }
   return (
     <ul className={styles.list}>
@@ -48,7 +16,7 @@ const Users = (props) => {
             <div>
               <img
                 className={styles.userAvatar}
-                src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg"
+                src={user.photos.small != null ? user.photos.small : userAvatarSmall}
                 width="40"
                 height="40"
                 alt="avatar-big"/>
@@ -60,12 +28,12 @@ const Users = (props) => {
             </div>
             <div className={styles.userInfo}>
               <p className={styles.userName}>{user.name}</p>
-              <p>{user.location.city + ', ' + user.location.country}</p>
+              <p>{'user.location.city + user.location.country'}</p>
               <p>{user.status}</p>
             </div>
           </li>
         ))
-      };
+      }
     </ul>
   );
 };
